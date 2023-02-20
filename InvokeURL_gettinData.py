2b21ed65a011b15736fdbbffbb5652daa435ce6b
Enrique Kishimoto <shiguemitsu@users.noreply.github.com>
2023-02-20 14:33:42 -0500

    Create InvokeURL_gettinData.py

---
+++ InvokeURL_gettinData.py
@@ -0,0 +1,17 @@
+from com.ibm.json.java import JSONObject, JSONArray
+
+# Invoking an URL
+url = "https://www.example.com/"
+userid = None
+passwd = None
+
+# Invoke URL
+response = service.httpgetasjson(url, userid, None, passwd)
+
+status = response.getStatusLine().getStatusCode()
+obj = JSONArray.parse(response.getEntity().getContent())
+
+if status == 200:
+	# OK
+else:
+	# error
